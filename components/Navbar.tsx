2b87bcdc9d60ba58a6c2f4b5affafa4986070224
Harshit004 <rajharshit04@gmail.com>
2025-03-07 19:24:13 +0530

    removed navbar scroll animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,24 +8,24 @@ export default function Navbar() {
   const [isVisible, setIsVisible] = useState(true)
   const [lastScrollY, setLastScrollY] = useState(0)
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const currentScrollY = window.scrollY
       
-      if (currentScrollY > lastScrollY) {
-        // Scrolling down
-        setIsVisible(false)
-      } else {
-        // Scrolling up
-        setIsVisible(true)
-      }
+  //     if (currentScrollY > lastScrollY) {
+  //       // Scrolling down
+  //       setIsVisible(false)
+  //     } else {
+  //       // Scrolling up
+  //       setIsVisible(true)
+  //     }
       
-      setLastScrollY(currentScrollY)
-    }
+  //     setLastScrollY(currentScrollY)
+  //   }
 
-    window.addEventListener('scroll', handleScroll, { passive: true })
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [lastScrollY])
+  //   window.addEventListener('scroll', handleScroll, { passive: true })
+  //   return () => window.removeEventListener('scroll', handleScroll)
+  // }, [lastScrollY])
 
   return (
     <header 
